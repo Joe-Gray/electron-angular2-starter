e263cd54084a89bd1ecef9f191444a4d3128fa4d
gbsjoe <gbsjoe@gmail.com>
2017-02-04 16:55:30 -0700

    added node packages and home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }                 from '@angular/core';
+import { BrowserModule }            from '@angular/platform-browser';
+import { FormsModule }              from '@angular/forms';
+import { HttpModule }               from '@angular/http';
 
-import { AppComponent }  from './app.component';
+import { AppComponent }             from './app.component';
+import { AppRoutingModule }         from './routing/app-routing.module';
+import { HttpService }              from './core/http.service';
+
+import { HomeComponent }            from './home/home.component';
+import { TestComponent }            from './test/test.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, FormsModule, HttpModule, AppRoutingModule ],
+  declarations: [ AppComponent, HomeComponent, TestComponent ],
+  providers:    [ HttpService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
