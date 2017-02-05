96e439c63e2763037a3ab49847811d7b5e1991b0
gbsjoe <gbsjoe@gmail.com>
2017-02-04 19:42:03 -0700

    added file upload to test page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,9 @@ import { BrowserModule }            from '@angular/platform-browser';
 import { FormsModule }              from '@angular/forms';
 import { HttpModule }               from '@angular/http';
 
-import { MenubarModule }                      from 'primeng/primeng';
+import { MenubarModule }            from 'primeng/primeng';
+import { GrowlModule }              from 'primeng/primeng';
+import { FileUploadModule }         from 'primeng/primeng';
 
 import { AppComponent }             from './app.component';
 import { AppRoutingModule }         from './routing/app-routing.module';
@@ -13,7 +15,7 @@ import { HomeComponent }            from './home/home.component';
 import { TestComponent }            from './test/test.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpModule, AppRoutingModule, MenubarModule ],
+  imports:      [ BrowserModule, FormsModule, HttpModule, AppRoutingModule, MenubarModule, GrowlModule, FileUploadModule ],
   declarations: [ AppComponent, HomeComponent, TestComponent ],
   providers:    [ HttpService ],
   bootstrap:    [ AppComponent ]
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -8,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
+  uploadedFiles: any[] = [];
+  logoName: string;
+
   constructor() { }
+
+  onBeforeSend(event: any) : void {
+    event.xhr.setRequestHeader("CustomHeader", "CustomHeaderValue");
+  }
+
+  onUpload(event: any) : void {
+    console.log(event);
+  }
 
   ngOnInit() {
   }
