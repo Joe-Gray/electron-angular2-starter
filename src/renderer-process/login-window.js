86ed87f1edf08fe2a33dd31b98d187dea4298120
Joe-Gray <joseph.gray@carvana.com>
2017-02-16 12:28:06 -0700

    go get a JWT from server

--- src/renderer-process/login-window.js
+++ src/renderer-process/login-window.js
@@ -12,7 +12,7 @@ const GOOGLE_PROFILE_URL = 'https://www.googleapis.com/userinfo/v2/me'
 const GOOGLE_CLIENT_ID = '540654859254-g4terfrh1oajuhv1u5oqej0dltjndfqp.apps.googleusercontent.com';
 const GOOGLE_REDIRECT_URI = 'market.mobile.dev:/oauth2redirect&';
 
-export async function googleSignIn () {
+export async function googleSignIn() {
   const code = await signInWithPopup()
   console.log(code);
   const tokens = await fetchAccessTokens(code)
@@ -23,17 +23,19 @@ export async function googleSignIn () {
     email,
     displayName: name,
     idToken: tokens.id_token,
+    accessToken: tokens.access_token
   }
 
   console.log(providerUser);
-  return mySignInFunction(providerUser)
+  return await mySignInFunction(tokens.access_token)
 }
 
-export function mySignInFunction(providerUser){
-
+export async function mySignInFunction(accessToken) {
+  var response = await axios.get(`https://localhost:44377/api/accounts/getapptoken/${accessToken}`);
+  console.log(response);
 }
 
-export function signInWithPopup () {
+export function signInWithPopup() {
   return new Promise((resolve, reject) => {
     const authWindow = new remote.BrowserWindow({
       width: 500,
@@ -50,21 +52,21 @@ export function signInWithPopup () {
     }
     const authUrl = `${GOOGLE_AUTHORIZATION_URL}?${qs.stringify(urlParams)}`
 
-function handleNavigation (url) {
-    const query = parse.parse(url, true).query
-    if (query) {
-    if (query.error) {
-        reject(new Error(`There was an error: ${query.error}`))
-    } else if (query.code) {
-        // Login is complete
-        authWindow.removeAllListeners('closed')
-        setImmediate(() => authWindow.close())
+    function handleNavigation(url) {
+      const query = parse.parse(url, true).query
+      if (query) {
+        if (query.error) {
+          reject(new Error(`There was an error: ${query.error}`))
+        } else if (query.code) {
+          // Login is complete
+          authWindow.removeAllListeners('closed')
+          setImmediate(() => authWindow.close())
 
-        // This is the authorization code we need to request tokens
-        resolve(query.code)
+          // This is the authorization code we need to request tokens
+          resolve(query.code)
+        }
+      }
     }
-    }
-}
 
     authWindow.on('closed', () => {
       // TODO: Handle this smoothly
@@ -83,21 +85,21 @@ function handleNavigation (url) {
   })
 }
 
-export async function fetchAccessTokens (code) {
+export async function fetchAccessTokens(code) {
   const response = await axios.post(GOOGLE_TOKEN_URL, qs.stringify({
     code,
     client_id: GOOGLE_CLIENT_ID,
     redirect_uri: GOOGLE_REDIRECT_URI,
     grant_type: 'authorization_code',
   }), {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  })
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    })
   return response.data
 }
 
-export async function fetchGoogleProfile (accessToken) {
+export async function fetchGoogleProfile(accessToken) {
   const response = await axios.get(GOOGLE_PROFILE_URL, {
     headers: {
       'Content-Type': 'application/json',
@@ -111,5 +113,5 @@ export async function fetchGoogleProfile (accessToken) {
 const newWindowBtn = document.getElementById('login-window')
 
 newWindowBtn.addEventListener('click', async function (event) {
-    var xyz = await googleSignIn()
+  var xyz = await googleSignIn()
 })
